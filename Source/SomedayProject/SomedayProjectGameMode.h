3c4a3beb4e0f205eaf46b623621c56387c23d004
Hyun2and <52280545+jeongopo@users.noreply.github.com>
2025-09-12 23:03:37 +0900

    초기 프로젝트 세팅, 에셋 정리

---
+++ Source/SomedayProject/SomedayProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SomedayProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASomedayProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASomedayProjectGameMode();
+};
+
+
+
