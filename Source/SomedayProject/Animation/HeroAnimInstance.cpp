ab52d9c303261773c714c3b7ac2f59cfaf4c5275
Hyun2and <52280545+jeongopo@users.noreply.github.com>
2025-10-28 00:51:34 +0900

    Feat : 점프 기능 구현중
    아직 어색한 구간이 있음

---
+++ Source/SomedayProject/Animation/HeroAnimInstance.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HeroAnimInstance.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "SomedayProject/SomedayProjectCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "SomedayProject/SPLogHelper.h"
+
+UHeroAnimInstance::UHeroAnimInstance(const FObjectInitializer& ObjectInitializer)
+{
+}
+
+void UHeroAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* ASC)
+{
+	GameplayTagPropertyMap.Initialize(this, ASC);
+}
+
+void UHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	//AnimInstance 도 Actor에 붙나?
+	if (AActor* OwningActor = GetOwningActor())
+	{
+		if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+		{
+			InitializeWithAbilitySystem(ASC);
+		}
+
+		if (UCharacterMovementComponent* TempComp = Cast<UCharacterMovementComponent>(OwningActor->GetComponentByClass(UCharacterMovementComponent::StaticClass())))
+		{
+			MoveComp = TempComp;
+		}
+	}
+}
+
+void UHeroAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (MoveComp)
+	{
+		Velocity = MoveComp->Velocity;
+		bHasAcceleratrion = MoveComp->GetCurrentAcceleration().Length() > 0;
+
+		bWalking = Velocity.Length() > 0 && bHasAcceleratrion;
+
+		//UE_LOG(LogSPDefault, Log, TEXT("Falling %d, Velocity 값 %f, Gravity 값 %f, Apex 변화 %f"), bFalling, MoveComp->Velocity.Z, MoveComp->GetGravityZ(),  TimeToJumpApex);
+		bJumping = MoveComp->IsFalling() && MoveComp->Velocity.Z > 0;
+
+		if (bJumping)
+		{
+			TimeToJumpApex = MoveComp->Velocity.Z / (MoveComp->GetGravityZ()) * (-1);
+		}
+		else
+		{
+			TimeToJumpApex = 0.f;
+		}
+	}
+}
---
+++ Source/SomedayProject/Animation/HeroAnimInstance.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AbilitySystemComponent.h"
+#include "HeroAnimInstance.generated.h"
+
+class UAbilitySystemComponent;
+class UCharacterMovementComponent;
+/**
+ * 
+ */
+UCLASS()
+class SOMEDAYPROJECT_API UHeroAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+	UHeroAnimInstance (const FObjectInitializer& ObjectInitializer);
+
+	virtual void InitializeWithAbilitySystem (UAbilitySystemComponent* ASC);
+
+protected:
+	virtual void NativeInitializeAnimation () override;
+	virtual void NativeUpdateAnimation (float DeltaSeconds) override;
+	
+protected:
+	// Gameplay tags that can be mapped to blueprint variables. The variables will automatically update as the tags are added or removed.
+	// These should be used instead of manually querying for the gameplay tags.
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+
+private:
+	UPROPERTY()
+	UCharacterMovementComponent* MoveComp;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "MoveVariable")
+	FVector Velocity;
+
+	UPROPERTY(BlueprintReadOnly, Category = "MoveVariable")
+	bool bHasAcceleratrion = false;
+
+	//Jump
+	UPROPERTY(BlueprintReadOnly, Category = "MoveVariable")
+	float TimeToJumpApex = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "MoveVariable")
+	bool bJumping = false;
+	
+
+	UPROPERTY(BlueprintReadOnly, Category = "MoveVariable")
+	bool bWalking = false;
+};
